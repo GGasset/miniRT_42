84f9febeed126c96bf8a83a83d7ae6ab9e1b7b68
GGasset <gassetgerman@gmail.com>
2025-05-22 18:10:09 +0200

    included and moved t_render_data to minilibx_funcs/h

---
+++ Headers/minilibx_funcs.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilibx_funcs.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggasset- <ggasset-@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-05-22 16:02:48 by ggasset-          #+#    #+#             */
+/*   Updated: 2025-05-22 16:02:48 by ggasset-         ###   ########student.  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINILIBX_FUNCS_H
+# define MINILIBX_FUNCS_H
+
+# ifndef WINDOW_WIDTH
+#  define WINDOW_WIDTH 300
+# endif
+# ifndef ASPECT_RATIO
+// 16:9 default aspect ratio
+#  define ASPECT_RATIO 1.77777
+# endif
+# ifndef MAX_BOUNCES
+#  define MAX_BOUNCES 3
+# endif
+
+typedef struct t_render_data
+{
+}		t_render_data;
+
+#endif
