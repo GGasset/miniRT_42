345d936b503729e5a862edd58e7b2edf61d51ed4
GGasset <gassetgerman@gmail.com>
2025-07-28 16:56:17 +0200

    modularized create ray, set create ray to norminette standard

--- RT/Rays/rays.c
+++ RT/Rays/rays.c
@@ -36,38 +36,42 @@ static void	get_viewport(t_camera camera, t_vec3 viewport[2])
 	viewport[1] = vec3(0, -(viewport_width / aspect_ratio), 0);
 }
 
+static t_vec3	get_pixel0_pos(t_camera c, size_t i, t_vec3 d[2])
+{
+	t_vec3	viewport[2];
+	t_vec3	upper_left;
+	t_vec3	upper_left_pixel_pos;
+
+	get_viewport(c, viewport);
+	d[0] = vec_sdiv(viewport[0], c.width);
+	d[1] = vec_sdiv(viewport[1], c.height);
+	upper_left = vec_sust(vec_sust(vec_sust(c.camera_pos,
+					vec3(0, 0, c.focal_len)),
+				vec_sdiv(viewport[0], 2)),
+			vec_sdiv(viewport[1], 2));
+	upper_left_pixel_pos = vec_sum(upper_left, vec_smul(
+				vec_sum(d[0], d[1]), .5));
+	return (upper_left_pixel_pos);
+}
+
 t_ray	create_ray(t_camera camera, size_t pixel_i)
 {
-	t_vec3 viewport[2];
-	get_viewport(camera, viewport);
+	t_vec3	pixel_delta[2];
+	t_vec3	upper_left_pixel_pos;
+	t_vec3	pixel_center;
+	t_vec3	pixel0_pos;
 
-	t_vec3 pixel_delta[2];
-	pixel_delta[0] = vec_sdiv(viewport[0], camera.width);
-	pixel_delta[1] = vec_sdiv(viewport[1], camera.height);
-
-	t_vec3 upper_left = vec_sust(vec_sust(vec_sust(camera.camera_pos,
-											vec3(0, 0, camera.focal_len)),
-										vec_sdiv(viewport[0], 2)),
-								vec_sdiv(viewport[1], 2));
-	t_vec3 upper_left_pixel_pos = vec_sum(upper_left, vec_smul(
-		vec_sum(pixel_delta[0], pixel_delta[1]), .5));
-	size_t x = pixel_i % camera.width;
-	size_t y = pixel_i / camera.width;
-	t_vec3 pixel_center = vec_sum(vec_sum(upper_left_pixel_pos, 
-		vec_smul(pixel_delta[0], x))
-		, vec_smul(pixel_delta[1], y));
-	return ray(camera.camera_pos, vec_sust(pixel_center, camera.camera_pos));
+	pixel0_pos = get_pixel0_pos(camera, pixel_i, pixel_delta);
+	pixel_center = vec_sum(vec_sum(pixel0_pos,
+				vec_smul(pixel_delta[0], pixel_i % camera.width)),
+			vec_smul(pixel_delta[1], pixel_i / camera.width));
+	return (ray(camera.camera_pos, vec_sust(pixel_center, camera.camera_pos)));
 }
 
 /*t_ray create_equidistant_ray(t_camera camera, size_t pixel_i)
 {
     t_data aspect_ratio = (t_data)camera.width / camera.height;
-    t_data viewport_height = 2.0;
-    t_data viewport_width = aspect_ratio * viewport_height;
-
-    // Ejes del viewport
-    t_vec3 horizontal = vec3(viewport_width, 0, 0);       // eje X
-    t_vec3 vertical = vec3(0, -viewport_height, 0);       // eje Y (hacia abajo)
+    t_data viewport_height = 2.0;pixel0_pos(camera, pixel_i, pixel_delta)0);
 
     // Dirección en la que mira la cámara (eje Z negativo)
     t_vec3 forward = vec3(0, 0, -1);
@@ -99,5 +103,3 @@ t_ray	create_ray(t_camera camera, size_t pixel_i)
 
     return ray(camera.camera_pos, direction);
 }*/
-
-
