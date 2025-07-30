537c4bad7335825bdc4ebee47c494ffd18e069e4
GGasset <gassetgerman@gmail.com>
2025-07-30 13:46:47 +0200

    added matrix to vector mult and rotate function

--- RT/vec3.c
+++ RT/vec3.c
@@ -1,6 +1,7 @@
 #include "../Headers/RT_headers/vec3.h"
 #include "../libft/libft.h"
 #include "stdio.h"
+#include "vec3.h"
 
 t_vec3	vec3(t_data x, t_data y, t_data z)
 {
@@ -128,6 +129,36 @@ t_vec3	cross_product(t_vec3 a, t_vec3 b)
     return (vec_smul(n_unitary(a, b), modulus(a) * modulus(b) * sin(tt)));
 }
 
+void	vec_matrix_mult(t_vec3 matrix_row, t_vec3 vec, t_vec3 *out, size_t r_i)
+{
+	t_data	row_value;
+
+	if (!out)
+		return ;
+	row_value = matrix_row.vs[0] * vec.vs[0];
+	row_value += matrix_row.vs[1] * vec.vs[1];
+	row_value += matrix_row.vs[2] * vec.vs[2];
+	out->vs[r_i] = row_value;
+}
+t_vec3	rotate(t_vec3 input, t_vec3 degrees)
+{
+	t_vec3	out;
+	t_vec3	tmp;
+
+	tmp = input;
+	vec_matrix_mult(vec3(1, 0, 0), tmp, &out, 0);
+	vec_matrix_mult(vec3(0, cos(x(degrees)), -sin(x(degrees))), tmp, &out, 1);
+	vec_matrix_mult(vec3(0, sin(x(degrees)), cos(x(degrees))), tmp, &out, 2);
+	tmp = out;
+	vec_matrix_mult(vec3(cos(y(degrees)), 0, sin(y(degrees))), tmp, &out, 0);
+	vec_matrix_mult(vec3(0, 1, 0), tmp, &out, 1);
+	vec_matrix_mult(vec3(-sin(y(degrees)), 0, cos(y(degrees))), tmp, &out, 2);
+	tmp = out;
+	vec_matrix_mult(vec3(cos(z(degrees)), -sin(z(degrees)), 0), tmp, &out, 0);
+	vec_matrix_mult(vec3(sin(z(degrees)), cos(z(degrees)), 0), tmp, &out, 1);
+	vec_matrix_mult(vec3(0, 0, 1), tmp, &out, 2);
+	return (out);
+}
 void test_cross_product(t_vec3 a, t_vec3 b)
 {
     t_vec3 c = cross_product(a, b);
