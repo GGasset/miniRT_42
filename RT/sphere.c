907b46c6556c6e45fbd037baa6b477ae09f21594
GGasset <gassetgerman@gmail.com>
2025-08-25 12:16:50 +0200

    modified hit_sphere for generalizable behaviour

--- RT/sphere.c
+++ RT/sphere.c
@@ -12,28 +12,20 @@ t_data	hit_sphere(t_hit_args *args)
 	double	discrim;
 	t_vec3	center2r_orig;
 	
-	t_object sphere = {
-		.coords = vec3(0, 0, 10),               // Plano en el origen
-		.rotation = vec3(0, 0, 0),         // Normal apuntando hacia +Y (plano horizontal)
-		.color = vec3(255.0, 255.0, 255.0)
-	};
-	sphere.sizes = malloc(1 * sizeof(t_data));
-	sphere.sizes[0] = 4.0;
-
+	t_object sphere = args->object;
 	
 	center2r_orig = vec_sust(sphere.coords, args->ray.orig);
 	// (printf("centr2orig: "), print_vec3(center2r_orig)); 
 	// (printf("direct: "), print_vec3(args->ray.direct)); 
 	a = dot(args->ray.direct, args->ray.direct);
 	h = dot(args->ray.direct, center2r_orig);
-	c = dot(center2r_orig, center2r_orig) - (sphere.sizes[0] * sphere.sizes[0]);
+	c = dot(center2r_orig, center2r_orig) - (sphere.sizes.vs[0] * sphere.sizes.vs[0]);
 	// printf("a: %lf,  h %lf,  c: %lf\n", a, h, c);
 	discrim = h * h - a * c;
 	// printf("discrim: %lf\n", discrim);
 	if (discrim < 0)
 	{
 		args->hit_info->did_hit = 0;
-		free(sphere.sizes);
 		return (-1.0);
 	}
 	else
@@ -43,7 +35,6 @@ t_data	hit_sphere(t_hit_args *args)
 		args->hit_info->hit_obj = args->object;
 		args->hit_info->p = vec_sum(args->ray.orig, vec_smul(args->ray.direct, args->hit_info->distance));
 		args->hit_info->normal = norm(vec_sust(args->hit_info->p, sphere.coords));
-		free(sphere.sizes);
 		return (args->hit_info->distance);
 	}
 }
