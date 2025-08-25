7c610e15eff4cb92a8ea463e13e3942340905841
GGasset <gassetgerman@gmail.com>
2025-08-25 14:01:09 +0200

    fixed bug

--- RT/sphere.c
+++ RT/sphere.c
@@ -34,7 +34,7 @@ int	hit_sphere(t_hit_args args)
 		args.hit_info->hit_obj = args.object;
 		args.hit_info->p = vec_sum(args.ray.orig, vec_smul(args.ray.direct, args.hit_info->distance));
 		args.hit_info->normal = norm(vec_sust(args.hit_info->p, sphere.coords));
-		args.hit_info->did_hit = args.hit_info->distance > 0;
+		args.hit_info->did_hit = 1;
 		return (args.hit_info->did_hit);
 	}
 }
