df92ac99f4f9e0550d3b42d846c93b0857edf61c
GGasset <gassetgerman@gmail.com>
2025-08-26 13:34:34 +0200

    added conversion from randians to degrees

--- RT/Rays/rays.c
+++ RT/Rays/rays.c
@@ -63,7 +63,7 @@ static t_ray	assemble_ray(t_camera camera, t_vec3 pixel_center)
 	/*degrees = vec3(acos(x(camera.rotation)), acos(y(camera.rotation)),
 		acos(z(camera.rotation)));
 	degrees = vec_smul(degrees, 180 / 3.14159);*/
-	degrees = camera.rotation;
+	degrees = vec_smul(camera.rotation, 3.14159 / 180);
 	out.direct = norm(rotate(out.direct, degrees));
 	return (out);
 }
