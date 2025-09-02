25b7346513474c4f3c3ace76d0bcc22c5851c01b
GGasset <gassetgerman@gmail.com>
2025-09-02 12:08:35 +0200

    modified random denominator

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int main()
 	render_d.scene.camera.rotation = vec3(0, 0, 0);
 	render_d.scene.camera.camera_pos = vec3(0, 0, 5);
 	render_d.scene.camera.focal_len = 1;
-	render_d.scene.ambient_light.brightness = 1;
+	render_d.scene.ambient_light.brightness = .0;
 	render_d.scene.ambient_light.color = 0xFFFF0000;
 
 	render_d.scene.light.coords = vec3(5, 0, 0);
