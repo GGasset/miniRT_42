a912e581289b4e523d9fd61829c796d68fa2306c
GGasset <gassetgerman@gmail.com>
2025-09-03 17:18:51 +0200

    started check_router

--- Parsing/check_line.c
+++ Parsing/check_line.c
@@ -11,3 +11,22 @@
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+int	check_router(char *line)
+{
+	char	c;
+	size_t	i;
+
+	if (!line)
+		return (0);
+	while (ft_isspace(*line))
+		line++;
+	c = *line;
+	if (c != 'A' && c != 'C' && c != 'L' && c != 's' && c != 'p' && c != 'c')
+		return ((printf("Error\nInvalid object id\n", line), 0));
+	if ((c == 's' && line[1] != 'p') || (c == 'p' && line[1] != 'l')
+		|| (c == 'c' && line[1] != 'y'))
+		return ((printf("Error\nInvalid object id\n", line), 0));
+	line += 1 + (c == 's' || c == 'p' || c == 'c');
+
+}
