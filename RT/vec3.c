c3e3a9c0a3d94c8af1e76cb674072f9c9852f897
GGasset <gassetgerman@gmail.com>
2025-09-15 18:35:51 +0200

    fixed bug and prefered negative angles rather than one greater than 180

--- RT/vec3.c
+++ RT/vec3.c
@@ -194,15 +194,16 @@ t_vec3 get_angles(t_vec3 a, t_vec3 b)
 	tmp[0] = vec3(0, y(a), z(a));
 	tmp[1] = vec3(0, y(b), z(b));
 	out.vs[0] = vec_angle(tmp[0], tmp[1]);
-	out.vs[0] += 180 * (x(a) > x(b));
+	out.vs[0] *= 1 - (2 * (x(a) > x(b)));
 	tmp[0] = vec3(x(a), 0, z(a));
 	tmp[1] = vec3(x(b), 0, z(b));
 	out.vs[1] = vec_angle(tmp[0], tmp[1]);
-	out.vs[1] += 180 * ((y(a) > y(b)));
+	out.vs[1] *= 1 - (2 * ((y(a) > y(b))));
 	tmp[0] = vec3(x(a), y(a), 0);
 	tmp[1] = vec3(x(b), y(b), 0);
 	out.vs[2] = vec_angle(tmp[0], tmp[1]);
-	out.vs[2] += 180 * ((z(a) > z(b)));
+	out.vs[2] *= 1 - (2 * ((z(a) > z(b))));
+	out = norm(out);
 	return (out);
 }
 
