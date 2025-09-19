e2df5be896522c9d9124239f2d0d7efe4673c46d
GGasset <gassetgerman@gmail.com>
2025-09-19 18:09:41 +0200

    added progress bar

--- RT/camera_render.c
+++ RT/camera_render.c
@@ -14,19 +14,50 @@
 #include "camera.h"
 #include "stdio.h"
 
+void print_loading_bar(size_t i, size_t pixel_count)
+{
+	size_t	bar_char_count;
+	t_data	percentage_completed;
+	size_t	j;
+
+	if (i % 500 && 0)
+		return ;
+	bar_char_count = 10;
+	percentage_completed = (t_data)i / (pixel_count);
+	printf("[");
+	j = 0;
+	while (j < bar_char_count)
+	{
+		if ((float)j / bar_char_count > percentage_completed)
+			printf(" ");
+		else
+			printf("#");
+		j++;
+	}
+	printf("] ");
+	printf("%i%%", (int)(percentage_completed * 100));
+	printf("\r");
+}
+
 int	render(t_render_data *render_d)
 {
+	size_t	pixel_count;
     size_t	i;
 	t_ray	ray;
 
+	printf("\n\n");
+	pixel_count = render_d->scene.camera.height * render_d->scene.camera.width;
 	i = 0;
-    printf("Render loop started...  \n");
-    while (i < render_d->scene.camera.height * render_d->scene.camera.width)
+    while (i < pixel_count)
     {
 		ray = create_ray(render_d->scene.camera, i);
 		((int *)render_d->img.addr)[i] = world_get_color(render_d, 0, i, ray);
         i++;
+		print_loading_bar(i, pixel_count);
 	}
-	//render_d->scene.camera.rotation.vs[0] += 90;
+	i = 0;
+	while (i++ < 100)
+		printf(" ");
+	printf("\n");
 	return (0);
 }
