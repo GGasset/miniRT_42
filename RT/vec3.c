c5c4fc75f26a57d9d73d95d7e858737668f8740b
GGasset <gassetgerman@gmail.com>
2025-10-13 14:48:50 +0200

    fixed bug

--- RT/vec3.c
+++ RT/vec3.c
@@ -197,17 +197,17 @@ t_vec3 get_angles(t_vec3 a, t_vec3 b)
 	tmp[0] = vec3(0, y(a), z(a));
 	tmp[1] = vec3(0, y(b), z(b));
 	out.vs[0] = vec_angle(tmp[0], tmp[1]);
-	if (z(a) > z(b) && y(a) > y(b))
+	if (z(a) > z(b) || y(a) > y(b))
 		out.vs[0] *= -1;
 	tmp[0] = vec3(x(a), 0, z(a));
 	tmp[1] = vec3(x(b), 0, z(b));
 	out.vs[1] = vec_angle(tmp[0], tmp[1]);
-	if (x(a) > x(b) && z(a) > z(b))
+	if (x(a) > x(b) || z(a) > z(b))
 		out.vs[1] *= -1;
 	tmp[0] = vec3(x(a), y(a), 0);
 	tmp[1] = vec3(x(b), y(b), 0);
 	out.vs[2] = vec_angle(tmp[0], tmp[1]);
-	if (x(a) > x(b) && y(a) > y(b))
+	if (x(a) > x(b) || y(a) > y(b))
 		out.vs[2] *= -1;
 	return (out);
 }
