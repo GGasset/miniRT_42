ac06f717f92aa14b921dce44959cc4d4c13c24aa
GGasset <gassetgerman@gmail.com>
2025-10-13 18:13:18 +0200

    made get_pixel_0_pos redeable

--- RT/Rays/rays.c
+++ RT/Rays/rays.c
@@ -45,10 +45,11 @@ static t_vec3	get_pixel0_pos(t_camera c, t_vec3 delta[2])
 	get_viewport(c, viewport);
 	delta[0] = vec_sdiv(viewport[0], c.width);
 	delta[1] = vec_sdiv(viewport[1], c.height);
-	upper_left = vec_sust(vec_sust(vec_sust(c.camera_pos,
-					vec3(0, 0, c.focal_len)),
-				vec_sdiv(viewport[0], 2)),
-			vec_sdiv(viewport[1], 2));
+	upper_left = vec3(0, 0, 1);
+	upper_left = element_mult(upper_left, c.rotation);
+	upper_left = vec_sust(c.camera_pos, upper_left);
+	upper_left = vec_sust(upper_left, vec_sdiv(viewport[0], 2));
+	upper_left = vec_sust(upper_left, vec_sdiv(viewport[1], 2));
 	upper_left_pixel_pos = vec_sum(upper_left, vec_smul(
 				vec_sum(delta[0], delta[1]), .5));
 	return (upper_left_pixel_pos);
