9087769384b8e00f18bbac17637fd1ca1130c435
GGasset <gassetgerman@gmail.com>
2025-10-14 12:27:51 +0200

    removed zoom hook

--- minilibx-funcs/hooks.c
+++ minilibx-funcs/hooks.c
@@ -12,40 +12,10 @@
 
 #include "minilibx_funcs.h"
 
-static double	fov_to_zoom(size_t fov)
-{
-	double	out;
-
-	out = fov;
-	out = 180 - out;
-	out /= 180.0;
-	return (out);
-}
-
 static int	key_hook(int keycode, t_render_data *render_d)
 {
 	if (keycode == 65307)
 		mlx_loop_end(render_d->mlx);
-	return (0);
-}
-
-static int	mouse_hook(int button, int x, int y, t_render_data *render_d)
-{
-	double	zoom;
-	double	centralizer;
-
-	zoom = fov_to_zoom(render_d->scene.camera.fov + x * 0 + y * 0);
-	centralizer = 2 * fabs(.5 - zoom) / 5;
-	centralizer *= zoom >= .5 && button == 4;
-	centralizer *= zoom < .5 && button == 5;
-	render_d->has_rendered = 0;
-	if (button == 5)
-		render_d->scene.camera.fov += .05 * 180 * zoom + centralizer;
-	else if (button == 4)
-		render_d->scene.camera.fov -= .05 * 180 * zoom + centralizer;
-	else
-		render_d->has_rendered = 1;
-	mlx_loop(render_d->mlx);
 	return (0);
 }
 
@@ -58,6 +28,5 @@ static int	close_hook(t_render_data *data)
 void	create_hooks(t_render_data *r_d)
 {
 	mlx_key_hook(r_d->win, key_hook, r_d);
-	mlx_mouse_hook(r_d->win, mouse_hook, r_d);
 	mlx_hook(r_d->win, 17, (1L << 17), close_hook, r_d);
 }
