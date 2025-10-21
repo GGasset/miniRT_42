7837256a3923fccdbb7a5b0016976420f78b759c
GGasset <gassetgerman@gmail.com>
2025-10-21 17:57:04 +0200

    added camera movement

--- minilibx-funcs/hooks.c
+++ minilibx-funcs/hooks.c
@@ -15,7 +15,26 @@
 static int	key_hook(int keycode, t_render_data *render_d)
 {
 	if (keycode == 65307)
+	{
 		mlx_loop_end(render_d->mlx);
+		return (0);
+	}
+	render_d->has_rendered = FALSE;
+	if (keycode == 65363)
+		render_d->scene.camera.camera_pos.vs[0] += MOVEMENT_SPD;
+	else if (keycode == 65361)
+		render_d->scene.camera.camera_pos.vs[0] -= MOVEMENT_SPD;
+	else if (keycode == 65362)
+		render_d->scene.camera.camera_pos.vs[2] += MOVEMENT_SPD;
+	else if (keycode == 65364)
+		render_d->scene.camera.camera_pos.vs[2] -= MOVEMENT_SPD;
+	else if (keycode == 32)
+		render_d->scene.camera.camera_pos.vs[1] += MOVEMENT_SPD;
+	else if (keycode == 122)
+		render_d->scene.camera.camera_pos.vs[1] -= MOVEMENT_SPD;
+	else
+		render_d->has_rendered = TRUE;
+	mlx_loop(render_d->mlx);
 	return (0);
 }
 
